412899e110e4ed1c7fc096bfe938c503be1dc12d
KyanPepper <kyankks1@gmail.com>
2023-08-21 08:48:43 -0700

    created empty Profile Page

---
+++ frontend/src/components/Profile.js
@@ -0,0 +1,50 @@
+import React, { Component } from "react";
+import { withRouter } from "./withRouter";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  Redirect,
+  Navigate,
+  useNavigate,
+} from "react-router-dom";
+import PracticeMode from "./PracticeMode";
+import ExamMode from "./ExamMode";
+import LoginPage from "./LoginPage";
+import {
+  Button,
+  Grid,
+  Typography,
+  TextField,
+  FormControl,
+  FormHelperText,
+  FormLabel,
+  colors,
+  Box,
+  Container,
+  AppBar,
+  Toolbar,
+} from "@mui/material";
+import { Radio, RadioGroup, FormControlLabel } from "@mui/material";
+import { alignProperty } from "@mui/material/styles/cssUtils";
+class LoginPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      practice: true,
+      exam: false,
+      questions: 10,
+      UserSigned: true
+    };
+  }
+  render(){
+    return(
+        <container></container>
+    );
+
+    
+  }
+}
+
+    export default withRouter(LoginPage)
